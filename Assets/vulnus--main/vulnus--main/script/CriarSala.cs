4629c3bb588590dc4b974098feb4b27672e2bb1f
ART0067830 <ART0067830@SEN577986>
2026-08-19 14:27:50 -0300

    mudanças nos dois botões

---
+++ Assets/vulnus--main/vulnus--main/script/CriarSala.cs
@@ -0,0 +1,10 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class TrocarCena : MonoBehaviour
+{
+    public void IrParaCena3()
+    {
+        SceneManager.LoadScene("cena 3");
+    }
+}
