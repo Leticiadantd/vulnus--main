3d1161df7e730028e1aebd1e6958a2da2eabaa60
ART0067830 <ART0067830@SEN577986>
2026-08-31 16:53:32 -0300

    mudanças no botão

--- Assets/vulnus--main/vulnus--main/script/CriarSala.cs
+++ Assets/vulnus--main/vulnus--main/script/CriarSala.cs
@@ -1,5 +1,6 @@
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using TMPro;
 
 public class CriarSala : MonoBehaviour
 {
@@ -7,10 +8,19 @@ public class CriarSala : MonoBehaviour
     public void Abrircena3()
     {
         SceneManager.LoadScene("cena 3");
+    }
 
-         
-           
+    public void CampoCriarSala(TMP_InputField CampoCriarSala)
+    {
+        // Impede de continuar se o campo estiver vazio
+        if (string.IsNullOrWhiteSpace(CampoCriarSala.text))
+        {
+            Debug.LogWarning("Digite um nome antes de começar!");
+            return;
+        }
 
-           
+        // Salva o nome para usar nas próximas cenas
+        PlayerPrefs.SetString("NomeJogador", CampoCriarSala.text.Trim());
+
     }
 }
--- Assets/vulnus--main/vulnus--main/script/gerenciadordenome.cs
+++ Assets/vulnus--main/vulnus--main/script/gerenciadordenome.cs
@@ -5,7 +5,9 @@ using UnityEngine.UI;
 public class GerenciadorDeNome : MonoBehaviour
 {
     public TMP_InputField campoDeNome;
+    public TMP_InputField CampoDeSenha;
     public Button jogar;
+    public Button criarSala;
     // Chame este método em um botão de "Salvar" ou no evento OnEndEdit do InputField
     public void SalvarNome()
     {
@@ -15,15 +17,17 @@ public class GerenciadorDeNome : MonoBehaviour
 
     public void quandoOCampoDeNomeMudar( )
     {
-        if (string.IsNullOrWhiteSpace(campoDeNome.text))
+        if (string.IsNullOrWhiteSpace(campoDeNome.text) || string.IsNullOrWhiteSpace(CampoDeSenha.text))
         {
              jogar.interactable = false; // Desativa o botão se o campo estiver vazio
+             criarSala.interactable = false; // Desativa o botão se o campo estiver vazio
             Debug.LogWarning("O campo de nome está vazio!");
             return;
         }
         else
         {
             jogar.interactable = true; // Ativa o botão se o campo tiver texto
+            criarSala.interactable = true; // Ativa o botão se o campo tiver texto
         }
 
     }
