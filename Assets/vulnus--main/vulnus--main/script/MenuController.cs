bc8e5e72709478f3e33990477500ede2c38ce723
ART0067830 <ART0067830@SEN577986>
2026-08-31 15:40:45 -0300

    NOME OBRIGATÓRIO FINAL

--- Assets/vulnus--main/vulnus--main/script/MenuController.cs
+++ Assets/vulnus--main/vulnus--main/script/MenuController.cs
@@ -1,11 +1,24 @@
+using TMPro;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class MenuController : MonoBehaviour
 {
+    public TMP_InputField campoDeNome;
+
     public void Jogar()
     {
-        SceneManager.LoadScene("cena 2");
+        // Impede de continuar se o campo estiver vazio
+        if (string.IsNullOrWhiteSpace(campoDeNome.text))
+        {
+            Debug.LogWarning("Digite um nome antes de começar!");
+            return;
+        }
 
+        // Salva o nome para usar nas próximas cenas
+        PlayerPrefs.SetString("NomeJogador", campoDeNome.text.Trim());
+
+        // Carrega a cena 2 do seu jogo
+        SceneManager.LoadScene("cena 2");
     }
 }
--- Assets/vulnus--main/vulnus--main/script/gerenciadordenome.cs
+++ Assets/vulnus--main/vulnus--main/script/gerenciadordenome.cs
@@ -1,14 +1,30 @@
 using UnityEngine;
 using TMPro;
+using UnityEngine.UI;
 
 public class GerenciadorDeNome : MonoBehaviour
 {
     public TMP_InputField campoDeNome;
-
+    public Button jogar;
     // Chame este método em um botão de "Salvar" ou no evento OnEndEdit do InputField
     public void SalvarNome()
     {
         string nomeJogador = campoDeNome.text;
         Debug.Log("Nome digitado: " + nomeJogador);
+    }
+
+    public void quandoOCampoDeNomeMudar( )
+    {
+        if (string.IsNullOrWhiteSpace(campoDeNome.text))
+        {
+             jogar.interactable = false; // Desativa o botão se o campo estiver vazio
+            Debug.LogWarning("O campo de nome está vazio!");
+            return;
+        }
+        else
+        {
+            jogar.interactable = true; // Ativa o botão se o campo tiver texto
+        }
+
     }
 }
